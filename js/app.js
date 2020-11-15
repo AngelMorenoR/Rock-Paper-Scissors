7433a7508af34c954c8e4d33c1ee17728e5e5c53
AngelMorenoR <59493370+AngelMorenoR@users.noreply.github.com>
2020-11-15 17:25:57 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,120 @@
+const choose = document.querySelectorAll('.choose')
+const score = document.querySelector('.score-wins')
+
+let points = 0
+let opt;
+let animationFight;
+
+choose.forEach(el => {
+    el.addEventListener('click', function (e) {
+        score.innerHTML = points
+        
+        const ene = randomChoose()
+        opt = this
+
+        checkWhoWin(opt, ene)
+
+        animationFight = gsap.to($('.align'), { scale: 0,  onComplete: () => showFight(ene, opt) })
+        
+        
+    })
+})
+
+function showFight(ene, opt) {
+    // layout styles
+    setStyles('.btn-play', {'display': 'flex', 'justify-content': 'center', 'align-items': 'center'})
+    $('.align').css('display', 'none')
+    $('.game').css('flex-direction', 'column')
+    $('.container-fight').css('display', 'flex')
+
+    // you style
+    setStyles('.you', {
+        'height': $(opt).css('height'),
+        'padding': $(opt).css('padding'),
+        'border-radius': $(opt).css('border-radius'),
+        'background': $(opt).css('background'),
+        'border': $(opt).css('border'),
+        'cursor': $(opt).css('cursor')
+    })
+    $('.you').attr({'src': opt.attributes[1].value})
+
+    // house style
+    setStyles('.house', {
+        'height': $(ene).css('height'),
+        'padding': $(ene).css('padding'),
+        'border-radius': $(ene).css('border-radius'),
+        'background': $(ene).css('background'),
+        'border': $(ene).css('border'),
+        'cursor': $(ene).css('cursor')
+    })
+    $('.house').attr('src', ene.attributes[1].value)
+
+    // animate you, ene
+    gsap.fromTo($('.you'), {scale: 0}, {scale: 1})
+    gsap.fromTo($('.house'), {scale: 0}, {scale: 1})
+
+    function setStyles(ele, arr) {
+        for (const key in arr) {
+            $(ele).css(key, arr[key])
+        }  
+    }
+}
+
+function randomChoose() {
+    const n = Math.round(Math.random() * 2)
+    return choose[n]
+}
+
+function checkWhoWin(opt, ene){
+    const myOpt = opt.classList[0]
+    const eneOpt = ene.classList[0]
+
+    if(myOpt === eneOpt){
+        $('.who-win').text('draw')
+    }else if(myOpt == 'rock' && eneOpt == 'scissors'){
+        $('.who-win').text('YOU WIN')
+        points++
+        score.innerHTML = points
+    }else if(myOpt == 'rock' && eneOpt == 'paper'){
+        $('.who-win').text('YOU LOSE')
+        points == 0 ? 0 :  points--
+        score.innerHTML = points
+    }else if(myOpt == 'paper' && eneOpt == 'scissors'){
+        $('.who-win').text('YOU LOSE')
+        points == 0 ? 0 :  points--
+        score.innerHTML = points
+    }else if(myOpt == 'paper' && eneOpt == 'rock'){
+        $('.who-win').text('YOU WIN')
+        points++
+        score.innerHTML = points
+    }else if(myOpt == 'scissors' && eneOpt == 'rock'){
+        $('.who-win').text('YOU LOSE')
+        points == 0 ? 0 :  points--
+        score.innerHTML = points
+    }else if(myOpt == 'scissors' && eneOpt == 'paper'){
+        $('.who-win').text('YOU WIN')
+        points++
+        score.innerHTML = points
+    }
+}
+
+// rules modal
+$('.close').on('click', () => {
+    $('.rules-modal').css('display', 'none')
+    $('.all').css('display', 'none')
+})
+$('.rules').on('click', () => {
+    $('.rules-modal').css('display', 'block')
+    $('.all').css('display', 'block')
+})
+// rules modal
+
+// btn play-again
+$('.play-again').on('click', () => {
+    $('.container-fight').hide()
+    $('.align').css('display', 'block')
+    $('.game').css('flex-direction', '')
+    $('.btn-play').css('display', 'none')
+    animationFight.reverse()
+})
+
